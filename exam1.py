a3bd100416b2aa9e5377de1df3bd5039cf7b7911
Lucia Ku <101294728+LuciaKu@users.noreply.github.com>
2022-03-09 23:55:40 -0500

    Add files via upload

---
+++ exam1.py
@@ -0,0 +1,27 @@
+temperature = float(input("Please enter the average temperature of the Marketing department.\n"))
+
+if temperature > 65:
+    print ("AC should be on\n")
+else:
+    print ("AC should be off\n")
+    
+temperature = float(input("Please enter the average temperature of the HR department.\n"))
+
+if temperature > 65:
+    print ("AC should be on\n")
+else: 
+    print ("AC should be off\n")
+    
+temperature = float(input("Please enter the average temperature of the Business department.\n"))
+
+if temperature > 65:
+    print ("AC should be on\n")
+else: 
+    print ("AC should be off\n")
+    
+temperature = float(input("Please enter ther average temperature of the Executive department.\n"))
+
+if temperature > 65:
+    print ("AC should be on\n")
+else:
+    print ("AC should be off\n")
